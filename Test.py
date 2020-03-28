c962cea728ab63e938d235e545ef12967d477882
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-03-28 12:03:26 +0200

    Test

---
+++ Test.py
@@ -0,0 +1,16 @@
+
+from math import *
+
+name = input("Hello put your name here: ")
+age = input("Hello put your age here: ")
+print(name)
+phrase = "Guillain Bisimwa"
+print(phrase.isalpha())
+print(phrase.index("i"))
+print(phrase.replace("Bisimwa","Fiston"))
+
+n = 20
+print("My favorite number, "+ str(n))
+print(pow(5,2))
+print(min(5,2))
+print(sqrt(36))
