52a6694feb8f3e98e327b6c37f321ffdfbc28897
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-03-28 12:04:13 +0200

    Understanding Dictionaries

---
+++ Dict.py
@@ -0,0 +1,12 @@
+# Key values
+mounts = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    0: "Jully",
+}
+print(mounts[0])
+print(mounts.get("Aprr", "Not a valid key"))
