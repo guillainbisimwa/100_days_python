13b335084e17e21f85a80cfcac4884eb97daf4ba
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-03-28 13:45:39 +0200

    2d list

---
+++ NestedList.py
@@ -0,0 +1,15 @@
+nbr_gird = [
+    [1,3,5],
+    [8,9,2],
+    [6,2,10],
+    [0]
+]
+
+print(nbr_gird[0][2])
+
+#nested forloop
+try:
+    n = int(input("Put an int: "))
+    print(n)
+except:
+    print("Invalid: ")
