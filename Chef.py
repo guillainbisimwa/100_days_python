458cc50c443f8ce7c44535c15b2b8ee81c1a1a3c
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-03-28 14:21:17 +0200

    Inheritance

---
+++ Chef.py
@@ -0,0 +1,4 @@
+from Student import Student
+class Chef(Student):
+    def mssg(self):
+        print("Here i'am ")
--- Class.py
+++ Class.py
@@ -1,7 +1,11 @@
 from Student import Student
+from Chef import Chef
 
-student1 = Student("Guillain", 28, "NTIC", 3.4, False)
+
+
+student1 = Chef("Guillain", 28, "NTIC", 3.4, False)
 student2 = Student("Eva", 25, "Business", 3.0, True)
 
 print(student2.name)
+student2.print_name("F")
 student1.print_name("M")
