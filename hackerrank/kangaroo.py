d8bf206c80ceade0c6f461aef23d9a80ed62c88d
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-05-17 08:10:40 +0200

    Completed kangaroo challange

--- hackerrank/kangaroo.py
+++ hackerrank/kangaroo.py
@@ -9,11 +9,24 @@ import sys
 # Complete the kangaroo function below.
 # https://www.hackerrank.com/challenges/kangaroo/problem
 def kangaroo(x1, v1, x2, v2):
+    if x1 < x2 and v2 > v1:
+        return "NO"
+    elif x2 < x1 and v1 > v2:
+        return "NO"
+    else:
+        while True:
+            x1 = x1 + v1
+            x2 = x2 + v2
+            if x1 == x2:
+                #print("End at :"+str(x1))
+                return "YES"
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    x1V1X2V2 = input().split()
+    #x1V1X2V2 = input().split()
+    x1V1X2V2 = [0,3,4,2]
+    #x1V1X2V2 = [2,4,0,3]
 
     x1 = int(x1V1X2V2[0])
 
@@ -24,7 +37,8 @@ if __name__ == '__main__':
     v2 = int(x1V1X2V2[3])
 
     result = kangaroo(x1, v1, x2, v2)
+    print(result)
 
-    fptr.write(result + '\n')
+    # fptr.write(result + '\n')
 
-    fptr.close()
+    # fptr.close()
